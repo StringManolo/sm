03bafe7047b50de1766850a179b45c88f76dc7d9
StringManolo <manuelvarelacaldas@gmail.com>
2020-12-31 03:36:11 +0100

    automated pushed by clasificador.js

--- clasificador.js
+++ clasificador.js
@@ -21,7 +21,11 @@ let fecha = {};
 
 [fecha.año, fecha.mes, fecha.dia] = noticia.fecha.split("-");
 
-let titular = base64("e", noticia.titular);
+let titular = base64("e", noticia.titular).replace(/\+/g, "-").replace(/\//g, "_").replace(/\=/g, ",");
+
+if (titular.length > 99) {
+  titular.length = 99; /* Max filesystem name chars */
+}
 
 console.log(`Fecha de la noticia:
 Año - ${fecha.año}
@@ -32,19 +36,44 @@ Dia - ${fecha.dia}
 console.log(`Buscando en el sistema de ficheros la ruta news/${fecha.año}/${fecha.mes}/`);
 
 
-
 let res = comando(`cat news/${fecha.año}/${fecha.mes}/${fecha.dia}/${titular}`);
 
 if (res === "") {
   console.log("El artículo es nuevo. Almacenándolo.");
 
+  comando(`mkdir -p news/${fecha.año}/${fecha.mes}/${fecha.dia}/`);
+
   let err = { errno: "dummy" };
 
   let fd = std.open(`news/${fecha.año}/${fecha.mes}/${fecha.dia}/${titular}`, "w", err)
 
-//  fd.puts(noticiaRaw);
+  fd.puts(noticiaRaw);
   console.log(err.errno);
+} else {
+  /* Leer archivo y comparar ultimaModif */
+  let ultModAlm = (JSON.stringify(res).ultimaModificacion || 0);
+
+  if (ultModAlm === noticia.ultimaModificacion) {
+    console.log("El artículo ya existe y no presenta cambios");
+  } else if(ultModAlm < noticia.ultimaModificacion) {
+    console.log("El articulo introducido contiene cambios con respecto al almacenado. Introduce Si para sobrescribir.");
+    res = std.in.getline();
+    if (/s/gi.test(res)) {
+      let fd = std.open(`news/${fecha.año}/${fecha.mes}/${fecha.dia}/${titular}`, "w");
+      fd.puts(noticiaRaw);
+    }
+  } else {
+    console.log("Estás introducciendo un artículo más antiguo que el almacenado. Introduce Si para sobrescribirlo de todas maneras.");
+    res = std.in.getline();
+    if (/s/gi.test(res)) {
+      let fd = std.open(`news/${fecha.año}/${fecha.mes}/${fecha.dia}/${titular}`, "w");
+      fd.puts(noticiaRaw);
+    }
+  }
 }
 
-
+console.log("Actualizar repo? S/N");
+if (/s/gi.test(std.in.getline())) {
+  comando(`git add --all && git commit -m "automated pushed by clasificador.js" && push`);
+}
 //console.log(`Noticia: ${noticia.autor}`);
