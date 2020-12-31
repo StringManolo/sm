15df9f2fa8d7acd4929f55ba534fb82b166fc5b3
StringManolo <manuelvarelacaldas@gmail.com>
2020-12-31 06:28:45 +0100

    automated pushed by clasificador.js

--- main.js
+++ main.js
@@ -3,15 +3,38 @@ import ff from "./ff.js";
 ff.activateShortcuts();
 ff.defineShortcut("_", alert);
 
-_("test");
+_(1);
 
-let getLastArticles = number => {
+let getLastArticles = (number=10) => {
   ff._GET("./availableFiles.json", function(res) {
-    _(JSON.parse(res));
+    let files = JSON.parse(res);
+    let fileNames = Object.keys(files);
+    let urls =[];
+    let aux = [];
+    for (let i in fileNames) {
+      aux = files[fileNames[i]].date.split("-");
+      urls.push(`${aux[0]*365 + aux[1] * 30 + aux[2]}#${files[fileNames[i]].url}`);
+    }
+
+    urls = urls.sort();
+    urls.length = number;
+
+    for (let i in urls) {
+      urls[i] = urls[i].substr(+urls[i].indexOf("#") + 1, urls[i].length);
+    }
+
+    _(urls);
+
+    Promise.all(urls.map(url => fetch(url)))
+    .then(resp => Promise.all( resp.map(r => r.text()) ))
+    .then(result => {
+      _(result);
+    });
+
   });
 };
 
-getLastArticles();
+getLastArticles(2);
 
-_(1);
-i
+
+
